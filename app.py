1426265058b0a527e0c3aae32d86957453829122
vaibhav-soni-learnapp <125342282+vaibhav-soni-learnapp@users.noreply.github.com>
2024-05-02 16:04:54 +0530

    Update app.py

--- app.py
+++ app.py
@@ -11,7 +11,7 @@ from datetime import datetime
 st.title('API Programme - Clicks Fetcher')
 
 # Authorization token
-auth_token = 'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1aWQiOiIxODUyZmZmNi02N2RlLTRiNjYtYmIwMy01NDJlY2Q4YmZmNzMiLCJhZG0iOnRydWUsImlhdCI6MTcxNDU1NjIyMCwiZXhwIjoxNzE0NjQyNjIwLCJhdWQiOiJwbGF0bzowLjAuMSIsImlzcyI6InZhcnNpdHktbGl2ZSJ9.JWM1QGDjvpQ6Dcl44B2k-J42hBiNET-450lF2EqYHMY'
+auth_token = 'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1aWQiOiIxODUyZmZmNi02N2RlLTRiNjYtYmIwMy01NDJlY2Q4YmZmNzMiLCJhZG0iOnRydWUsImlhdCI6MTcxNDY0MjU5MSwiZXhwIjoxNzE0NzI4OTkxLCJhdWQiOiJwbGF0bzowLjAuMSIsImlzcyI6InZhcnNpdHktbGl2ZSJ9.qCXRtC38BFCwvqEWO8FDax5aQO_uvGCwekEk8kiZc14'
 headers = {'Authorization': auth_token}
 
 # Function to fetch clicks data
